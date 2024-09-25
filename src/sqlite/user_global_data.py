d047fb8cd3c4cb29e8a28ffd2f34714a744f8ec3
neveix <neveix2003@mail.ru>
2024-09-25 19:41:19 +0500

    bug fix

--- src/sqlite/user_global_data.py
+++ src/sqlite/user_global_data.py
@@ -11,7 +11,7 @@ class UserGlobalData:
         cur.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ({column_defs})")
         cur.close()
         self.con.commit()
-    def add_column(self, column_def):
+    def add_column(self, column_def: str):
         cur = self.con.cursor()
         cur.execute(f"ALTER TABLE {self.table_name} ADD COLUMN {column_def}")
         cur.close()
@@ -22,39 +22,39 @@ class UserGlobalData:
         cur = self.con.cursor() 
         result = cur.execute(f"SELECT {fields} FROM {self.table_name} WHERE user_id = {user_id}").fetchall()
         return result
-    def set(self, user_id: int, field: str, value: Any):
+    def set(self, user_id: int, field: str, value):
         cur = self.con.cursor() 
         row_count = cur.execute(f"SELECT count() FROM {self.table_name} WHERE user_id = {user_id}").fetchall()[0][0]
         if row_count == 0:
-            cur.execute(f"INSERT INTO {self.table_name} ({field}) VALUES (?)", value)
+            cur.execute(f"INSERT INTO {self.table_name} (user_id,{field}) VALUES ({user_id},?)", (value,))
         else:
-            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", value)
+            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", (value,))
         cur.close()
         self.con.commit()
     def add(self, user_id: int, field: str, value: Any):
         cur = self.con.cursor() 
         row_count = cur.execute(f"SELECT count() FROM {self.table_name} WHERE user_id = {user_id}").fetchall()[0][0]
         if row_count == 0:
-            cur.execute(f"INSERT INTO {self.table_name} ({field}) VALUES (?)", value)
+            cur.execute(f"INSERT INTO {self.table_name} (user_id,{field}) VALUES ({user_id},?)", (value,))
         else:
-            cur.execute(f"UPDATE {self.table_name} SET {field} = {field} + ? WHERE user_id = {user_id}", value)
+            cur.execute(f"UPDATE {self.table_name} SET {field} = {field} + ? WHERE user_id = {user_id}", (value,))
         cur.close()
         self.con.commit()
     def add_to_list(self, user_id: int, field: str, value: Any):
         cur = self.con.cursor() 
-        row_count = cur.execute(f"SELECT count() FROM {self.table_name} WHERE user_id = {user_id}").fetchall()[0][0]
+        selected = cur.execute(f"SELECT {field} FROM {self.table_name} WHERE user_id = {user_id}").fetchall()
         from json import dumps
         from json import loads
-        if row_count == 0:
+        if len(selected) == 0:
             list = [value]
             list_str = dumps(list)
-            cur.execute(f"INSERT INTO {self.table_name} ({field}) VALUES (?)", list_str)
+            cur.execute(f"INSERT INTO {self.table_name} (user_id,{field}) VALUES ({user_id},?)", (list_str,))
         else:
-            list_str = cur.execute(f"SELECT {field} FROM {self.table_name} WHERE user_id = {user_id}").fetchall()[0][0]
+            list_str = selected[0][0]
             List = loads(list_str)
             List.append(value)
             list_str = dumps(List)
-            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", list_str)
+            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", (list_str,))
         cur.close()
         self.con.commit()
     def remove_from_list(self, user_id: int, field: str, value: Any):
@@ -67,16 +67,16 @@ class UserGlobalData:
             List: list = loads(list_str)
             List.remove(value)
             list_str = dumps(List)
-            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", list_str)
+            cur.execute(f"UPDATE {self.table_name} SET {field} = ? WHERE user_id = {user_id}", (list_str,))
         cur.close()
         self.con.commit()
-    def delete_user(self, user_id: int, sql: str):
+    def delete_user(self, user_id: int, sql: str = ""):
         """executes: DELETE FROM {self.table_name} WHERE user_id = {user_id} {sql} """
         cur = self.con.cursor()
         cur.execute(f"DELETE FROM {self.table_name} WHERE user_id = {user_id} {sql}")
         cur.close()
         self.con.commit()
-    def select(self, columns: str, sql: str):
+    def select(self, columns: str="*", sql: str = ""):
         """returns: cur.execute(f"SELECT {columns} FROM {self.table_name} {sql}").fetchall()"""
         cur = self.con.cursor() 
         return cur.execute(f"SELECT {columns} FROM {self.table_name} {sql}").fetchall()
