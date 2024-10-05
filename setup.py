caeeb936e24a6991427a14a8ad329f0cca3bce60
neveix <neveix2003@mail.ru>
2024-10-05 23:53:58 +0500

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='tg_bot_base',
+   version='1.0.0',
+   description='It is useful for creating simple telegram bots',
+   author='Neveix',
+   author_email='neveix2003@mail.ru',
+   packages=['tg_bot_base'],
+   install_requires=['python-telegram-bot']
+)
