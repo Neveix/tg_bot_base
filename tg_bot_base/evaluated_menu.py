023ff7ac81447cbceee98efb9533bedcbb0a08d2
neveix <neveix2003@mail.ru>
2024-10-14 19:02:13 +0500

    screen manager implemented edit_screen

--- tg_bot_base/evaluated_menu.py
+++ tg_bot_base/evaluated_menu.py
@@ -1,20 +1,27 @@
-from telegram import Bot, InlineKeyboardMarkup, InputMediaPhoto
+from telegram import Bot, InlineKeyboardMarkup, InputMediaPhoto, Message
 
 class EvaluatedMenu:
-    def __init__(self, text: str = None, reply_markup: InlineKeyboardMarkup = None, photos: list[InputMediaPhoto] = None):
+    def __init__(self, text: str = None, reply_markup: InlineKeyboardMarkup = None, 
+            photos: list[InputMediaPhoto] = None):
         self.text = text
         self.reply_markup = reply_markup
         self.photos = photos
+        self.sended_message: Message = None
     def clone(self) -> "EvaluatedMenu":
         if isinstance(self, EvaluatedMenuDefault):
             return EvaluatedMenuDefault.clone(self)
         else:
             return EvaluatedMenuPhoto.clone(self)
-    def send(self, bot: Bot, chat_id: int):
+    async def send(self, bot: Bot, chat_id: int):
         if isinstance(self, EvaluatedMenuDefault):
-            EvaluatedMenuDefault.send(self, bot, chat_id)
+            await EvaluatedMenuDefault.send(self, bot, chat_id)
         else:
-            EvaluatedMenuPhoto.send(self, bot, chat_id)
+            await EvaluatedMenuPhoto.send(self, bot, chat_id)
+    async def edit_message(self, bot: Bot, chat_id: int, message_id: int):
+        if isinstance(self, EvaluatedMenuDefault):
+            await EvaluatedMenuDefault.send(self, bot, chat_id)
+        else:
+            await EvaluatedMenuPhoto.send(self, bot, chat_id)
 
 
 class EvaluatedMenuDefault(EvaluatedMenu):
@@ -22,12 +29,18 @@ class EvaluatedMenuDefault(EvaluatedMenu):
         super().__init__(text = text,reply_markup = reply_markup)
     def clone(self) -> "EvaluatedMenuDefault":
         return EvaluatedMenuDefault(self.text, self.reply_markup)
-    def send(self, bot: Bot, chat_id: int):
-        bot.send_message(chat_id, text = self.text, reply_markup = self.reply_markup)
+    async def send(self, bot: Bot, chat_id: int):
+        self.sended_message = await bot.send_message(chat_id, text = self.text, reply_markup = self.reply_markup)
+    async def edit_message(self, bot: Bot, chat_id: int, message_id: int):
+        self.sended_message = await bot.edit_message_text(text = self.text, chat_id = chat_id, message_id = message_id)
+        await bot.edit_message_reply_markup(reply_markup = self.reply_markup, chat_id = chat_id, message_id = message_id)
+        
 class EvaluatedMenuPhoto(EvaluatedMenu):
     def __init__(self, photos: list[InputMediaPhoto]):
         super().__init__(photos=photos)
     def clone(self) -> "EvaluatedMenuPhoto":
         return EvaluatedMenuPhoto(self.photos)
-    def send(self, bot: Bot, chat_id: int):
-        bot.send_media_group(chat_id, self.photos)
+    async def send(self, bot: Bot, chat_id: int):
+        self.sended_message = await bot.send_media_group(chat_id, self.photos)[0]
+    async def edit_message(self, bot: Bot, chat_id: int, message_id: int):
+        self.sended_message = await bot.edit_message_media(media = self.photos[0], chat_id = chat_id, message_id = message_id)
