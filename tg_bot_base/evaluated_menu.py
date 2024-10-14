c1e1acf9b35d3bea3a6e67b07d11b0df6a05bcda
neveix <neveix2003@mail.ru>
2024-10-14 17:00:24 +0500

    Evaluated menu clone method added

--- tg_bot_base/evaluated_menu.py
+++ tg_bot_base/evaluated_menu.py
@@ -1,13 +1,25 @@
 from telegram import InlineKeyboardMarkup, InputMediaPhoto
 
 class EvaluatedMenu:
-    pass
+    def __init__(self, text: str = None, reply_markup: InlineKeyboardMarkup = None, photos: list[InputMediaPhoto] = None):
+        self.text = text
+        self.reply_markup = reply_markup
+        self.photos = photos
+    def clone(self) -> "EvaluatedMenu":
+        if isinstance(self, EvaluatedMenuDefault):
+            return EvaluatedMenuDefault.clone(self)
+        else:
+            return EvaluatedMenuPhoto.clone(self)
+
 
 class EvaluatedMenuDefault(EvaluatedMenu):
     def __init__(self, text: str, reply_markup: InlineKeyboardMarkup):
-        self.text = text
-        self.reply_markup = reply_markup
+        super().__init__(text = text,reply_markup = reply_markup)
+    def clone(self) -> "EvaluatedMenuDefault":
+        return EvaluatedMenuDefault(self.text, self.reply_markup)
 
 class EvaluatedMenuPhoto(EvaluatedMenu):
-    def __init__(self, photos: list[InputMediaPhoto] | None):
-        self.photos = photos
+    def __init__(self, photos: list[InputMediaPhoto]):
+        super().__init__(photos=photos)
+    def clone(self) -> "EvaluatedMenuPhoto":
+        return EvaluatedMenuPhoto(self.photos)
--- tg_bot_base/screen_manager.py
+++ tg_bot_base/screen_manager.py
@@ -1,12 +1,13 @@
 
 from .bot_manager import BotManager
+from .evaluated_menu import EvaluatedMenu, EvaluatedMenuDefault, EvaluatedMenuPhoto
 
 class ScreenManager:
     def __init__(self, bot_manager: BotManager):
         self.bot_manager = bot_manager
-    def get_screen(self):
-        pass
-    def set_screen(self, screen):
-        pass
-    def update_screen(self):
-        pass
+        self.screen = None
+    def get_screen(self) -> list[EvaluatedMenu] | None:
+        """If self.screen is not None, returns copy if list of Evaluated Menus."""
+        return [evaluated_menu.clone() for evaluated_menu in self.screen]
+    def set_screen(self, screen: list[EvaluatedMenu]):
+        self.screen = screen
