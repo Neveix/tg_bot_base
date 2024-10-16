8e52b98d8119ea48ec37a6beb79d7115f797b182
neveix <neveix2003@mail.ru>
2024-10-17 00:26:01 +0500

    error raising added

--- tg_bot_base/screen.py
+++ tg_bot_base/screen.py
@@ -3,11 +3,16 @@ from .evaluated_screen import EvaluatedScreen
 
 class Screen:
     def __init__(self, name: str, *menus: list[Menu]):
+        if not isinstance(menus, list):
+            raise ValueError(f"{menus=} is not list")
         self.name = name
         self.menus: list[Menu] = []
         self.extend(menus)
     def extend(self, *menus: list[Menu]):
-        self.menus.extend(menus)
+        for menu in menus:
+            if not isinstance(menu, Menu):
+                raise ValueError(f"{menu=} is not of type {Menu}")
+            self.menus.append(menu)
     def to_evaluated_screen(self, **kwargs) -> EvaluatedScreen:
         return EvaluatedScreen(
             [menu.to_evaluated_menu(**kwargs) for menu in self.menus]
