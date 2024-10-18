f7edd92e5c52f925f092a18504de09c5d178783b
neveix <neveix2003@mail.ru>
2024-10-18 19:50:12 +0500

    UserDataManager self.user_data_type field added, bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup # type: ignore
 
 setup(
    name='tg_bot_base',
-   version='1.3.0',
+   version='1.3.1',
    description='It is useful for creating simple telegram bots',
    author='Neveix',
    author_email='neveix2003@mail.ru',
--- tg_bot_base/user_data.py
+++ tg_bot_base/user_data.py
@@ -15,16 +15,18 @@ class UserData:
         from .evaluated_screen import EvaluatedScreen
         self.screen: EvaluatedScreen = None
 
-
 class UserDataManager:
     def __init__(self, bot_manager: "BotManager"):
         self.bot_manager = bot_manager
+        self.user_data_type = UserData``
         self.__users_data: dict[str, UserData] = {}
     def get(self, user_id: int) -> UserData:
         user_data = self.__users_data.get(user_id)
         if user_data is None:
-            user_data = UserData(user_id)
+            user_data = self.user_data_type(user_id)
             self.__users_data[user_id] = user_data
         return user_data
-        
+    def reset(self, user_id: int) -> None:
+        user_data = self.user_data_type(user_id)
+        self.__users_data[user_id] = user_data
         
