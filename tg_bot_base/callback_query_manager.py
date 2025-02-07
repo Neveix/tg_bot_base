bf55cdcdd98a987cff171ecbe6efb071b36714a6
Neveix <87336789+Neveix@users.noreply.github.com>
2025-02-07 11:00:29 +0500

    cosmetic fix

--- tg_bot_base/callback_query_manager.py
+++ tg_bot_base/callback_query_manager.py
@@ -11,10 +11,12 @@ class CallbackQueryManager:
     def __init__(self, bot_manager: BotManager):
         self.dummy_handle_func: Callable[[Update, CallbackContext], None]
         self.bot_manager: BotManager = bot_manager
+        
         async def callback_query_handler(update: Update, context: CallbackContext):
             query = update.callback_query
             user_id: int = query.from_user.id
             await query.answer()
+            
             callback_data = self.bot_manager.user_data_manager.get(user_id).callback_data
             if not callback_data:
                 self.dummy_handle_func(update, context)
@@ -22,14 +24,17 @@ class CallbackQueryManager:
             if query.data not in callback_data:
                 self.dummy_handle_func(update, context)
                 return
+            
             data: CallbackData = callback_data[query.data]
             if data.action == "menu":
                 screen_name: str = data.args[0]
                 if not isinstance(screen_name, str):
                     raise ValueError(f"{screen_name=} is not str")
                 await bot_manager.user_screen_manager.set_user_screen_by_name(user_id, screen_name)
+            
             elif data.action == "step_back":
                 await bot_manager.user_screen_manager.step_back(user_id)
+            
             elif data.action == "function":
                 function = data.args[0]
                 if not callable(function):
@@ -37,8 +42,10 @@ class CallbackQueryManager:
                 await function(bot_manager=self.bot_manager,
                     update=update, context=context, user_id=user_id, *data.args[1:], **data.kwargs)
                 await update.callback_query.answer()
+            
             elif data.action == "url":
                 url = data.kwargs["url"]
         self.callback_query_handler = callback_query_handler
+    
     def get_handler(self) -> CallbackQueryHandler:
         return CallbackQueryHandler(self.callback_query_handler)
