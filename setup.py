2cb11d955022ea327ed3da6fde6dfc716923354b
Neveix <87336789+Neveix@users.noreply.github.com>
2025-03-24 10:24:21 +0500

    v1.4.1

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
+import sys
 from setuptools import setup # type: ignore
 
+extras_require = {
+   "ptb": [
+      # Дополнительные зависимости для расширения
+      "python-telegram-bot[job-queue]>=21.9",
+   ],}
+
+package_data = {
+   "my_library": ["*.py"],
+}
+
+if any("ptb" in arg for arg in sys.argv):
+   package_data["my_library"].extend(["ext/*.py", "ext/ptb/*.py"])
+
+
 setup(
-   name='tg_bot_base',
-   version='1.4.0',
-   description='It is useful for creating simple telegram bots',
-   author='Neveix',
-   author_email='neveix2003@mail.ru',
-   packages=['tg_bot_base'],
+   name="tg_bot_base",
+   version="1.4.1",
+   description="It is useful for creating simple telegram bots",
+   author="Neveix",
+   author_email="neveix2003@mail.ru",
+   packages=["tg_bot_base"],
    install_requires=[],
-   extras_require={
-      'ptb': [
-         # Дополнительные зависимости для расширения
-         'python-telegram-bot[job-queue]>=21.9',
-      ],
-   },
+   extras_require=extras_require,
+   package_data=package_data
 )
